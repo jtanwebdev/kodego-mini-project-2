fae494378bf4303beb387c1b7fa0f9061b3e60df
jtanwebdev <joeltandigital@gmail.com>
2024-03-20 02:09:36 +0800

    first commit

---
+++ src/Components/NewsItem.jsx
@@ -0,0 +1,24 @@
+import image from "../assets/news.jpg";
+const NewsItem = ({ title, description, src, url }) => {
+  return (
+    <div
+      className="card bg-dark text-light d-inline-block mb-3 my-3 mx-4 px-2 py-2"
+      style={{ maxWidth: "345px" }}
+    >
+      <img src={src ? src : image} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{title.slice(0, 50)}</h5>
+        <p className="card-text">
+          {description
+            ? description.slice(0, 90)
+            : "News is a report of a current event. It is information about something that has just happened."}
+        </p>
+        <a href={url} className="btn btn-primary">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
